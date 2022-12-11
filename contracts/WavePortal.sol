d99cb5d44e396adfed7946b8ffecdeabcd2c7626
Manny <manueljosetorres02@gmail.com>
2022-12-11 16:17:49 -0600

    test

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,6 +7,12 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+
+    /*
+     * We will be using this below to help generate a random number
+     */
+    uint256 private seed;
+
     /*
      * A little magic, Google what events are in Solidity!
      */
@@ -30,6 +36,11 @@ contract WavePortal {
 
     constructor() payable {
         console.log("This is constructed...");
+
+        seed = (block.timestamp + block.difficulty) % 100;
+
+        console.log("Random # generated: %d", seed);
+
     }
 
     /*
@@ -43,22 +54,39 @@ function wave(string memory _message) public {
 
     waves.push(Wave(msg.sender, _message, block.timestamp));
 
-    emit NewWave(msg.sender, block.timestamp, _message);
+ /*
+     * Generate a new seed for the next user that sends a wave
+     */
+    seed = (block.difficulty + block.timestamp + seed) % 100;
 
-    uint256 prizeAmount = 0.0001 ether;
-    require(
+    console.log("Random # generated: %d", seed);
+
+ /*
+    /* Give a 50% chance that the user wins the prize.
+     */
+    if (seed <= 50) {
+        console.log("%s won!", msg.sender);
+
+        /*
+        /* The same code we had before to send the prize.
+        */
+        uint256 prizeAmount = 0.0001 ether;
+        require(
         prizeAmount <= address(this).balance,
-        "Trying to withdraw more money than the contract has."
-    );
-    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-    require(success, "Failed to withdraw money from contract.");
+            "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+    emit NewWave(msg.sender, block.timestamp, _message);
 }
 
     /*
      * I added a function getAllWaves which will return the struct array, waves, to us.
      * This will make it easy to retrieve the waves from our website!
      */
-    function getAllWaves() public view returns (Wave[] memory) {
+   function getAllWaves() public view returns (Wave[] memory) {
         return waves;
     }
 
